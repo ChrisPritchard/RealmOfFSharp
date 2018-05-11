ded457d142373d55b2d6fdb2e200117424aa3a77
Christopher <chrispritchard.nz@gmail.com>
2018-05-11 13:00:14 +1200

    Created a sub function for rendering a health bar

--- orc-battle/View.fs
+++ orc-battle/View.fs
@@ -3,11 +3,18 @@ open GameCore
 
 let playerAnims = [0..9] |> List.map (fun i -> (i * 32,0,32,32))
 
+let barElements colourKey rect percent = 
+    seq {
+        yield { textureKey = "black"; destRect = rect; sourceRect = None }
+        let (x,y,width,height) = rect
+        yield { textureKey = "white"; destRect = x+2,y+2,width-4,height-4; sourceRect = None }
+        let valueWidth = width - 4 |> float |> (*) percent |> int
+        yield { textureKey = colourKey; destRect = x+2,y+2,valueWidth,height-4; sourceRect = None }
+    } |> Seq.toList
+
 let getView runState _ = 
     let playerFrame = playerAnims.[(runState.elapsed / 100.0) % 10.0 |> int]
     [
         //{ textureKey = "player"; destRect = 100,100,100,100; sourceRect = Some playerFrame }
-        { textureKey = "black"; destRect = 100,100,400,30; sourceRect = None }
-        { textureKey = "green"; destRect = 102,102,300,26; sourceRect = None }
-        { textureKey = "white"; destRect = 402,102,96,26; sourceRect = None }
-    ], []
+        barElements "green" (100,100,400,30) 0.5
+    ] |> List.concat, []
