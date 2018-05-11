6bdb7044f54e7d1fcc92005849059f1189243301
Christopher <chrispritchard.nz@gmail.com>
2018-05-12 10:39:37 +1200

    Interim work on orc controller

--- orc-battle/Controller.fs
+++ orc-battle/Controller.fs
@@ -10,15 +10,26 @@ let initialBattle = {
     state = turnStart
 }
 
+let handlePlayerTurn (runState: RunState) playerState battle =
+    if playerState.actionsRemaining = 0 then
+        if runState.WasJustPressed Keys.Enter then
+            Some { battle with state = OrcTurn { index = 0; lastTick = 0.0 } }
+        else
+            Some battle
+    else
+        if runState.WasJustPressed Keys.S then playerAttack Stab playerState battle |> Some
+        elif runState.WasJustPressed Keys.F then playerAttack Flail playerState battle |> Some
+        elif runState.WasJustPressed Keys.R then playerAttack Recover playerState battle |> Some
+        else Some battle
+
+let handleOrcTurn orcState battle =
+    Some battle
+
 let updateModel (runState: RunState) currentBattle = 
     match currentBattle with
     | None -> Some initialBattle
     | Some battle ->
         match battle.state with
         | GameOver -> currentBattle
-        | PlayerTurn playerState ->
-            if runState.WasJustPressed Keys.Left then
-                Some battle
-            else Some battle
-        | OrcTurn orcIndex -> 
-            Some battle
+        | PlayerTurn playerState -> handlePlayerTurn runState playerState battle
+        | OrcTurn orcState -> handleOrcTurn runState orcState battle
