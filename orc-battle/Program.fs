9bc6cda6f52a7149fe0650d0fc5507d1f383558b
Chris Pritchard <chris.pritchard@edpay.nz>
2018-05-16 14:25:16 +1200

    Resolution for orc battle set

--- orc-battle/Program.fs
+++ orc-battle/Program.fs
@@ -11,6 +11,6 @@ open GameCore
 
 [<EntryPoint>]
 let main _ =
-    use game = new GameCore<BattleModel>(View.assets, Controller.updateModel, View.getView)
+    use game = new GameCore<BattleModel>(Windowed (800,600), View.assets, Controller.updateModel, View.getView)
     game.Run()
     0
