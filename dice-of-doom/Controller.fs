1155a42f7aeee2266142f5833720a76e18b22e50
Chris Pritchard <chris.pritchard@edpay.nz>
2018-05-17 15:04:19 +1200

    Fixed broken controller code

--- dice-of-doom/Controller.fs
+++ dice-of-doom/Controller.fs
@@ -1,9 +1,17 @@
 module Controller
 open Model
 
-let initialModel = { playerIndex = 0 }
+let initialModel = { 
+    player = 0
+    source = None
+    target = None
+    gameTree = generateTree startTerritories 0 false
+}
+
+let checkForMoves runState gameModel =
+    gameModel
 
 let updateModel runState currentModel = 
     match currentModel with
     | None -> Some initialModel
-    | Some model -> Some model
+    | Some model -> checkForMoves runState model |> Some
