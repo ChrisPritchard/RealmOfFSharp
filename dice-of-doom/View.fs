0892f2195b4db80cd5b8a6eda973e6a2545e5d8a
Chris Pritchard <chris.pritchard@edpay.nz>
2018-05-18 13:45:10 +1200

    Added rendering of pass button

--- dice-of-doom/View.fs
+++ dice-of-doom/View.fs
@@ -16,9 +16,10 @@ let assets =
         Texture { key = "hex_pointy"; path = "Content/hexPointy" }
         Font { key = "default"; path = "Content/JuraMedium" }
         Texture { key = "pointer"; path = "Content/pointer" }
+        Texture { key = "white"; path = "Content/white" }
     ]
 
-let passButton = (50,50,200,50)
+let passButton = (50,50,150,60)
 
 let private rectFrom (ox,oy) (x,y) = 
     (ox + x) - hexWidth/2.0 |> int, 
@@ -36,9 +37,7 @@ let calculateOffset board =
 let getView runState model =
 
     // hexes
-        // colour
         // fading on select / options
-    // dice numbers
     // pass button visibility
     // player turn display
     // game over / reset
@@ -50,17 +49,30 @@ let getView runState model =
     let texture = match hexTop with | Flat -> "hex_flat" | _ -> "hex_pointy"
 
     let hexDisplay = hexes |> List.collect (fun (territory,point) ->
-        let drawable = { assetKey = texture; destRect = rectFrom (ox,oy) point; sourceRect = None }
         let (px,py) = point
         let labelPos = (int (px + ox), int (py + oy))
         let diceLabel = ColouredText (Color.White, { assetKey = "default";text = string territory.dice;position = labelPos;origin = Centre;scale=1. })
+
+        let drawable = { assetKey = texture; destRect = rectFrom (ox,oy) point; sourceRect = None }
         match territory.owner with
         | 0 -> 
             [ColouredImage (Color.Red, drawable); diceLabel]
         | _ -> 
             [ColouredImage (Color.Blue, drawable); diceLabel])
+    
+    let passButton = 
+        let canPass = Option.bind (List.tryPick (fun (m,_) -> match m with | Pass -> Some m | _ -> None)) model.gameTree.moves 
+        match canPass with
+        | None -> []
+        | Some _ ->
+            let (px,py,pw,ph) = passButton
+            [
+                ColouredImage (Color.Black, { assetKey = "white"; destRect = passButton; sourceRect = None })
+                ColouredImage (Color.White, { assetKey = "white"; destRect = (px+2,py+2,pw-4,ph-4); sourceRect = None })
+                Text { assetKey = "default";text = "Pass";position = (px + pw/2,py + ph/2);origin = Centre;scale=0.6 }
+            ]
 
     let (mx,my) = runState.mouse.position
     let cursor = Image { assetKey = "pointer"; destRect = mx,my,27,27; sourceRect = Some (0,0,18,18) }         
 
-    hexDisplay @ [cursor]
+    hexDisplay @ passButton @ [cursor]
