705e1a1f82db604151c1f467092ed604072ca813
Christopher <chrispritchard.nz@gmail.com>
2018-05-20 17:18:15 +1200

    AI now searches at a given depth

--- dice-of-doom/AI.fs
+++ dice-of-doom/AI.fs
@@ -3,7 +3,7 @@ open Model
 open GameCore
 
 let timeBetweenActions = 1000.
-let searchDepth = 4
+let searchDepth = 3
 
 let score board player =
     match winners board with
@@ -11,10 +11,29 @@ let score board player =
     | lst when List.contains player lst -> 0.5
     | _ -> 0. 
 
+let rec scoreAll moves depth player = 
+    match depth with
+    | 0 -> 0.
+    | _ ->
+        moves |> List.sumBy (fun (_,gt) -> 
+            let gta = gt ()
+            let boardScore = score gta.board player
+            let subScore = 
+                match gta.moves with
+                | Some m -> scoreAll m (depth - 1) player
+                | None -> 0.
+            boardScore + subScore)
+
 let aiMove (runState:RunState) model (moves: (Move * (unit -> GameTree)) list) =
     if runState.elapsed-model.lastAIAction < timeBetweenActions then
         model
     else 
         let player = model.gameTree.player
-        let (_,gt) = moves |> List.maxBy (fun (_,gt) -> score (gt().board) player)
+        let (_, gt) = 
+            moves |> List.maxBy (fun (_,gt) -> 
+            let gta = gt ()
+            let boardScore = score gta.board player
+            match gta.moves with
+            | None -> boardScore
+            | Some sm -> boardScore + scoreAll sm searchDepth player)
         { model with lastAIAction = runState.elapsed; gameTree = gt () }
